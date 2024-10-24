04851828456ceb9ca44641d4c98f9a61d7c13a5d
Chahine <chahine.mabrouk.bouzouita@gmail.com>
2024-10-24 14:47:22 +0200

    main comments

--- src/Main.java
+++ src/Main.java
@@ -34,18 +34,13 @@ public class Main {
             System.out.println("Usage: java Main <input_file>");
             System.exit(1);
         }
-        File input = new File(argv[0]);//on peut juste renseigner le chemin du fichier
+        File input = new File(argv[0]);
         if (!input.exists()){
             System.out.println("File not found: "+argv[0]);
             System.exit(1);
         }
         try(FileReader reader = new FileReader(input)){
             LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer(reader);
-
-            // Redirige la sortie standard vers le fichier avec l'extension .out
-            // PrintStream originalOut = System.out;
-            // PrintStream fileOut = new PrintStream(argv[0].replace("pmp","out"));
-            // System.setOut(fileOut);
 
             Symbol symbol = lexicalAnalyzer.nextSymbol();
             while(symbol.getType() != LexicalUnit.EOS){
@@ -54,8 +49,7 @@ public class Main {
                 symbol = lexicalAnalyzer.nextSymbol();
             }
             printSymbolTable(); //print the symbol table
-            // Rétablit la sortie standard
-            // System.setOut(originalOut);
+            
         }
         catch(Exception e){
             System.out.println("Error while opening file: "+argv[0]);
